5c39e9cf11cbe29bb22d8254d6d900542dd87fa1
bokkaa <bokyh0327@gmail.com>
2024-03-26 19:52:14 +0900

    dd

---
+++ src/test/java/com/example/amazon_1/Amazon1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amazon_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Amazon1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
